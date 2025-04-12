f3ec44058289770b7fa363562d984366c4e43307
Leo、逆天改命 <83278781+LiangGuangzhou@users.noreply.github.com>
2025-04-12 12:44:55 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const MAX_HISTORY_ITEMS = 5;
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
+    name: '老梁TV',
     url: 'https://libretv.is-an.org',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
