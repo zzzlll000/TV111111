d38e5ccebdf19acd6f83ee15d169e786a0121dd4
xiaoshengbaozi <57822974+xiaoshengbaozi@users.noreply.github.com>
2025-04-15 09:40:00 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const MAX_HISTORY_ITEMS = 5;
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
+    name: 'BaoziTV',
     url: 'https://libretv.is-an.org',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
@@ -192,4 +192,4 @@ const CUSTOM_API_CONFIG = {
 };
 
 // 新增隐藏内置黄色采集站API的变量，默认为true
-const HIDE_BUILTIN_ADULT_APIS = true;
+const HIDE_BUILTIN_ADULT_APIS = false;
