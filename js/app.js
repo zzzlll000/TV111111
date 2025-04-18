eeeecdccb952d20954bbe93ed003684a2c137892
guangxuemu <90951018+guangxuemu@users.noreply.github.com>
2025-04-18 19:39:55 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 全局变量
-let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["heimuer"]'); // 默认选中黑木耳
+let selectedAPIs = JSON.parse(localStorage.getItem('selectedAPIs') || '["ruyi","cjhw","ffzy","dbzy","lzzy","bfzy","qiqikp","heimuer","mozhua","mdzy","wolong","tyyszy","ikunzy","zy360","wujin","zuida","jisu"]'); // 默认选中黑木耳
 let customAPIs = JSON.parse(localStorage.getItem('customAPIs') || '[]'); // 存储自定义API列表
 
 // 添加当前播放的集数索引
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 仅选择黑木耳源
-        selectedAPIs = ["heimuer"];
+        selectedAPIs = ["ruyi","cjhw","ffzy","dbzy","lzzy","bfzy","qiqikp","heimuer","mozhua","mdzy","wolong","tyyszy","ikunzy","zy360","wujin","zuida","jisu"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
         
         // 默认选中过滤开关
